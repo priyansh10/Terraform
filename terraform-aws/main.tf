d15bb5de1d6acbfe79abfcb6c488db1d833f0b8f
Ubuntu <ubuntu@ip-172-31-44-98.ap-south-1.compute.internal>
2023-05-15 23:15:59 +0000

    terraform aws

---
+++ terraform-aws/main.tf
@@ -0,0 +1,40 @@
+terraform {
+	required_providers {
+	aws = {
+	source = "hashicorp/aws"
+	version = "~> 4.16"
+}
+}
+
+required_version =">=1.2.0"
+}
+
+
+provider "aws" {
+	region = "ap-south-1"
+}
+
+resource "aws_instance" "my_ec2_instance" {
+	ami = "ami-02eb7a4783e7e9317"
+	instance_type = "t2.micro"
+	tags = {
+		Name = var.instance_name
+}
+}
+
+
+/*resource "aws_s3_bucket" "my_s3_bucket" {
+	bucket = "terraform-priyansh-s3-1234"
+	tags = {
+	Name = "terraform-priyansh-s3-1234"
+	Environment = "Dev"
+}
+}*/
+
+
+
+
+
+output "ec2_public_ips" {
+	value = aws_instance.my_ec2_instance[*].public_ip
+}
---
+++ terraform-aws/variables.tf
@@ -0,0 +1,7 @@
+variable "instance_name" {
+	description = "value of the name tag for the EC2 instance"
+	type        = string
+	default     = "Example-terraform-instance" 
+
+
+}
